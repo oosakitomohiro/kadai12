754af4111e5aa50ee2f0874bd418c639ebc83afd
oosakitomohito <t.oosaki.tb@gmail.com>
2020-04-28 11:57:47 +0000

    課題13

--- 課題1.php
+++ 課題1.php
@@ -1,8 +1,8 @@
 <?php
 
-$name="oosaki tomohiro";
-if($name == "oosaki tomohiro"){
-    echo "私はあなたの名前です";
-}else{
-    echo "あなたの名前ではありません";
+function ka($dai){
+    return  $dai * 2 ;
 }
+echo ka()."\n";
+
+?>
